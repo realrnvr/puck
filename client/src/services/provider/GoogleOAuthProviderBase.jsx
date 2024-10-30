110357e2052d5a188b04d29937f005d583894563
realrnvr <realrnvr@gmail.com>
2024-10-30 23:10:14 +0530

    env updates

--- client/src/services/provider/GoogleOAuthProviderBase.jsx
+++ client/src/services/provider/GoogleOAuthProviderBase.jsx
@@ -3,9 +3,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export const GoogleOAuthProviderBase = ({ children }) => {
   return (
-    <GoogleOAuthProvider
-      clientId={import.meta.env.VITE_REACT_APP_GOOGLE_CLIENT_ID}
-    >
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       {children}
     </GoogleOAuthProvider>
   );
