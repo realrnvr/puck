b496abcbb9f164d249fb3f0b74d35dbf21e425d4
realrnvr <realrnvr@gmail.com>
2024-11-06 00:33:52 +0530

    temp multiple request fix + (updates in future)

--- client/src/pages/auth/redirect/Redirect.jsx
+++ client/src/pages/auth/redirect/Redirect.jsx
@@ -1,44 +1,45 @@
 import "./redirect.css";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../hooks/useAuth";
 import { axiosInstance } from "../../../services/api/axios";
 import Loader from "../../../components/ui/loader/Loader";
+import { useMutation } from "@tanstack/react-query";
 
 const Redirect = () => {
   const auth = useAuth();
   const navigate = useNavigate();
+
+  const isMutating = useRef(false);
+
+  const { mutate: googleMutate } = useMutation({
+    mutationFn: (code) => axiosInstance.post("/api/v1/auth/google", { code }),
+    onSuccess: (data) => {
+      console.log("Response from backend:", data.data);
+      auth.setToken(data.data?.accessToken);
+      navigate("/account");
+      isMutating.current = false;
+    },
+    onError: (error) => {
+      console.error("Error during Google login:", error);
+      navigate("/login");
+      isMutating.current = false;
+    },
+  });
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const code = params.get("code");
     console.log("Authorization code:", code);
 
-    if (code) {
-      handleGoogleLogin(code);
-    } else {
+    if (code && !isMutating.current) {
+      isMutating.current = true;
+      googleMutate(code);
+    } else if (!code) {
       console.error("No authorization code found in the URL.");
       navigate("/login");
     }
-  }, [navigate]);
-
-  const handleGoogleLogin = async (code) => {
-    try {
-      const response = await axiosInstance.post(
-        "http://localhost:3000/api/v1/auth/google",
-        {
-          code,
-        }
-      );
-      console.log("Response from backend:", response.data);
-      const { accessToken } = response.data;
-      auth.setToken(accessToken);
-      navigate("/account");
-    } catch (error) {
-      console.error("Error during Google login:", error);
-      navigate("/login");
-    }
-  };
+  }, [navigate, googleMutate]);
 
   return (
     <div className="redirect">
