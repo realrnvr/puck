f2031a8698e8124b7d48bda360239b3da583ecd2
realrnvr <realrnvr@gmail.com>
2024-11-14 19:15:23 +0530

    read component

--- client/src/pages/read/Read.jsx
+++ client/src/pages/read/Read.jsx
@@ -1,13 +1,13 @@
 import "./read.css";
+import { Link } from "react-router-dom";
+import MangaCard from "../../components/ui/mangaCard/MangaCard";
 
 const Read = () => {
   return (
     <>
-      <article className="read | container">
-        <div className="read__top-wrapper">
-          <div className="read__img-wrapper">
-            <img src="/anime-bg.webp" className="read__img" alt="" />
-          </div>
+      <article className="read">
+        <div className="read__top-wrapper | container">
+          <img src="/anime-bg.webp" className="read__img" alt="" />
           <div className="read__intro-container">
             <h2 className="read__title">READ</h2>
             <p className="read__description">
@@ -15,6 +15,24 @@ const Read = () => {
               each panel a universe waiting to be discovered. Ready to turn the
               page? Your adventure starts now!
             </p>
+          </div>
+        </div>
+        {/* manga pieces */}
+        <div className="read__manga-wrapper">
+          <div className="read__manga | container">
+            <h3 className="read__manga-title">Read manga series!</h3>
+            <div className="read__manga-container">
+              <MangaCard img={"berserk-manga-cover"} title={"Berserk"} />
+              <MangaCard img={"vaga-bond-manga-cover"} title={"Vaga Bond"} />
+              <MangaCard
+                img={"vinland-saga-manga-cover"}
+                title={"Vinland Saga"}
+              />
+              <MangaCard img={"monster-manga-cover"} title={"Monster"} />
+            </div>
+            <Link to="/goto" className="read__link">
+              See all manga series
+            </Link>
           </div>
         </div>
         {/* <div>manga here</div> */}
