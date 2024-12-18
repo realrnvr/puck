55cab4a4b019ff8c058c952e1e32d0a993e113f3
realrnvr <realrnvr@gmail.com>
2024-12-18 17:09:13 +0530

    manga routes

---
+++ server/controller/manga.js
@@ -0,0 +1,70 @@
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/badRequestError.js";
+import { notFound } from "../middleware/notFound.js";
+import axios from "axios";
+
+const BASE_URL = "https://api.mangadex.org";
+
+export const statics = async (req, res) => {
+  const { mangaId } = req.params;
+  if (!mangaId) {
+    throw new BadRequestError("Please provide params");
+  }
+
+  try {
+    const response = await axios.get(`${BASE_URL}/manga/${mangaId}`);
+    if (!response.data) {
+      throw new notFound("Manga is not there");
+    }
+
+    res.status(StatusCodes.OK).json({ data: response.data.data });
+  } catch (error) {
+    throw new BadRequestError("something went wrong");
+  }
+};
+
+export const author = async (req, res) => {
+  const { authorId } = req.params;
+
+  if (!authorId) {
+    throw new BadRequestError("Please provide author id");
+  }
+
+  try {
+    const response = await axios.get(`${BASE_URL}/author/${authorId}`);
+
+    if (!response.data) {
+      throw new notFound("author is not there");
+    }
+    res.status(StatusCodes.OK).json({ data: response.data.data });
+  } catch (error) {
+    throw new BadRequestError("something went wrong");
+  }
+};
+
+export const cover = async (req, res) => {
+  const { mangaId } = req.params;
+
+  if (!mangaId) {
+    throw new BadRequestError("provide manga Id");
+  }
+
+  try {
+    const response = await axios.get(`${BASE_URL}/cover`, {
+      params: {
+        manga: [mangaId],
+        limit: 2,
+        order: {
+          volume: "desc",
+        },
+        includes: ["manga"],
+      },
+    });
+    console.log(response.data);
+    const fileName = response.data.data[0]?.attributes?.fileName;
+    const coverImgUrl = `https://uploads.mangadex.org/covers/${mangaId}/${fileName}.512.jpg`;
+    res.status(200).json({ coverImgUrl });
+  } catch (error) {
+    throw new BadRequestError("something went wrong");
+  }
+};
