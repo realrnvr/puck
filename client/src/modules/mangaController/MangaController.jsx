5952732a148d258868432ea69177630b4f318e0a
realrnvr <realrnvr@gmail.com>
2024-12-27 16:23:36 +0530

    Quality mode

--- client/src/modules/mangaController/MangaController.jsx
+++ client/src/modules/mangaController/MangaController.jsx
@@ -20,11 +20,18 @@ export function MangaController({
       CHUNK_SIZE,
       totalChapters,
     },
+    quality,
+    setQuality,
   },
 }) {
   const [isOpen, setIsOpen] = useState(false);
   const hasChapterPrev = chapterCount === 0;
   const hasChapterNext = chapterCount === chapters?.length - 1;
+
+  const handleQualityChange = (e) => {
+    const { value } = e.target;
+    setQuality(value);
+  };
 
   return (
     <>
@@ -53,68 +60,60 @@ export function MangaController({
         )}
       </button>
       <div className={`controller ${isOpen ? "controller-close" : null}`}>
-        <div className="controller__container-left">
-          <div>
-            <label htmlFor="read">You are reading, by:</label>
-            <select name="read" id="read">
-              <option value="chapter">Chapter</option>
-            </select>
-          </div>
-          <select name="language" id="language">
-            <option value="en">language: EN</option>
-            <option value="ja">Language: JA</option>
-          </select>
-          <p>setting</p>
-        </div>
         <div className="controller__container-right">
           <div className="controller__nav-panel">
-            <div className="controller__btn-wrapper">
-              <button
-                type="button"
-                className={`controller__nav-btn ${
-                  hasChapterPrev ? "disabled__btn" : null
-                }`}
-                disabled={hasChapterPrev}
-                onClick={() =>
-                  setChapterCount((prevChapterCount) => prevChapterCount - 1)
-                }
-              >
-                <svg
-                  className="controller__nav-svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
+            <div className="controller__top-container">
+              <div>
+                <span className="controller__chap-count">{totalChapters}</span>
+              </div>
+              <div className="controller__btn-wrapper">
+                <button
+                  type="button"
+                  className={`controller__nav-btn ${
+                    hasChapterPrev ? "disabled__btn" : null
+                  }`}
+                  disabled={hasChapterPrev}
+                  onClick={() =>
+                    setChapterCount((prevChapterCount) => prevChapterCount - 1)
+                  }
                 >
-                  <path d="M18.464 2.114a.998.998 0 00-1.033.063l-13 9a1.003 1.003 0 000 1.645l13 9A1 1 0 0019 21V3a1 1 0 00-.536-.886zM17 19.091L6.757 12 17 4.909v14.182z" />
-                </svg>
-              </button>
-              <button type="button" className="controller__list-btn">
-                <span>Chapter - {currChapter}</span>
-                <svg
-                  className="controller__list-svg"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
+                  <svg
+                    className="controller__nav-svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                  >
+                    <path d="M18.464 2.114a.998.998 0 00-1.033.063l-13 9a1.003 1.003 0 000 1.645l13 9A1 1 0 0019 21V3a1 1 0 00-.536-.886zM17 19.091L6.757 12 17 4.909v14.182z" />
+                  </svg>
+                </button>
+                <button type="button" className="controller__list-btn">
+                  <span>Chapter - {currChapter}</span>
+                  <svg
+                    className="controller__list-svg"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M16 8A8 8 0 110 8a8 8 0 0116 0zM8.5 4.5a.5.5 0 00-1 0v5.793L5.354 8.146a.5.5 0 10-.708.708l3 3a.5.5 0 00.708 0l3-3a.5.5 0 00-.708-.708L8.5 10.293V4.5z" />
+                  </svg>
+                </button>
+                <button
+                  type="button"
+                  className={`controller__nav-btn ${
+                    hasChapterNext ? "disabled__btn" : null
+                  }`}
+                  disabled={hasChapterNext}
+                  onClick={() =>
+                    setChapterCount((prevChapterCount) => prevChapterCount + 1)
+                  }
                 >
-                  <path d="M16 8A8 8 0 110 8a8 8 0 0116 0zM8.5 4.5a.5.5 0 00-1 0v5.793L5.354 8.146a.5.5 0 10-.708.708l3 3a.5.5 0 00.708 0l3-3a.5.5 0 00-.708-.708L8.5 10.293V4.5z" />
-                </svg>
-              </button>
-              <button
-                type="button"
-                className={`controller__nav-btn ${
-                  hasChapterNext ? "disabled__btn" : null
-                }`}
-                disabled={hasChapterNext}
-                onClick={() =>
-                  setChapterCount((prevChapterCount) => prevChapterCount + 1)
-                }
-              >
-                <svg
-                  className="controller__nav-svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M5.536 21.886a1.004 1.004 0 001.033-.064l13-9a1 1 0 000-1.644l-13-9A.998.998 0 005 3v18a1 1 0 00.536.886zM7 4.909L17.243 12 7 19.091V4.909z" />
-                </svg>
-              </button>
+                  <svg
+                    className="controller__nav-svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                  >
+                    <path d="M5.536 21.886a1.004 1.004 0 001.033-.064l13-9a1 1 0 000-1.644l-13-9A.998.998 0 005 3v18a1 1 0 00.536.886zM7 4.909L17.243 12 7 19.091V4.909z" />
+                  </svg>
+                </button>
+              </div>
             </div>
             <div className="controller__controls">
               <div className="controller__pag">
@@ -176,6 +175,25 @@ export function MangaController({
                 })}
               </ul>
             </div>
+          </div>
+        </div>
+        <div className="controller__container-left">
+          <div className="controller__drop-down">
+            <label htmlFor="quality">Quality</label>
+            <select
+              className="controller__select"
+              name="quality"
+              id="quality"
+              value={quality}
+              onChange={handleQualityChange}
+            >
+              <option className="controller__option" value="data">
+                Original
+              </option>
+              <option className="controller__option" value="data-saver">
+                Data saver
+              </option>
+            </select>
           </div>
         </div>
       </div>
