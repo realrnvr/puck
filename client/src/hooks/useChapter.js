0a375e9df448dd24cb30178c1e6e0f12c40b0145
realrnvr <realrnvr@gmail.com>
2024-12-30 16:21:42 +0530

    Custom chapter hook : [All about chapter by mangaId]

---
+++ client/src/hooks/useChapter.js
@@ -0,0 +1,106 @@
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "../services/api/axios";
+import { useCallback, useMemo, useState } from "react";
+
+const CHUNK_SIZE = 100;
+
+export const useChapter = (mangaId) => {
+  // state
+  const [chapterCount, setChapterCount] = useState(0);
+  const [quality, setQuality] = useState("data");
+  const [offset, setOffset] = useState(0);
+
+  // chapter data
+  const { data: chapter } = useQuery({
+    queryKey: ["chapter", { mangaId, CHUNK_SIZE, offset }],
+    queryFn: () =>
+      axiosInstance.get(
+        `/api/v1/manga/chapters/${mangaId}?limit=${CHUNK_SIZE}&offset=${offset}`
+      ),
+    placeholderData: keepPreviousData,
+  });
+
+  // data extraction
+  const chapters = chapter?.data?.data;
+  const totalChapters = chapter?.data?.total;
+  const chapterBound = {
+    first: chapter?.data?.chapterBound?.first,
+    last: chapter?.data?.chapterBound?.last,
+  };
+
+  const chapterId = chapters && chapters[chapterCount]?.id;
+  const currChapter = chapters && chapters[chapterCount]?.attributes?.chapter;
+  const currVolume = chapters && chapters[chapterCount]?.attributes?.volume;
+
+  // chapter limit
+  const hasChapterPrev = chapterCount === 0;
+  const hasChapterNext = chapterCount === chapters?.length - 1;
+
+  // chapter images
+  const { data, isLoading } = useQuery({
+    queryKey: ["chapter-image", { chapterId, quality }],
+    queryFn: () =>
+      axiosInstance.get(`/api/v1/manga/chapter-image/${chapterId}`, {
+        params: {
+          quality: quality,
+        },
+      }),
+    placeholderData: keepPreviousData,
+    enabled: !!chapterId,
+  });
+
+  // image array
+  const slides = isLoading ? [] : data?.data?.data;
+
+  // memo chunk val
+  const hasPrevChunk = useMemo(() => offset > 0, [offset]);
+  const hasNextChunk = useMemo(
+    () => offset + CHUNK_SIZE < totalChapters,
+    [offset, totalChapters]
+  );
+
+  // handle chunk
+  const handlePrevChunk = useCallback(() => {
+    if (hasPrevChunk) {
+      setOffset((prev) => prev - CHUNK_SIZE);
+      setChapterCount(0);
+    }
+  }, [hasPrevChunk]);
+
+  const handleNextChunk = useCallback(() => {
+    if (hasNextChunk) {
+      setOffset((prev) => prev + CHUNK_SIZE);
+      setChapterCount(0);
+    }
+  }, [hasNextChunk]);
+
+  // handle quality
+  const handleQualityChange = (e) => {
+    const { value } = e.target;
+    setQuality(value);
+  };
+
+  return {
+    chapter: {
+      chapterCount,
+      setChapterCount,
+      quality,
+      handleQualityChange,
+      currChapter,
+      currVolume,
+      chapters,
+      totalChapters,
+      chapterBound,
+      hasChapterPrev,
+      hasChapterNext,
+    },
+    nav: {
+      hasPrevChunk,
+      handlePrevChunk,
+      hasNextChunk,
+      handleNextChunk,
+    },
+    slides,
+    isLoading,
+  };
+};
