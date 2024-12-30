71817df81c74cdb8e0b63122096e6175b9de202a
realrnvr <realrnvr@gmail.com>
2024-12-30 16:22:29 +0530

    folder structure changes + default import

---
+++ client/src/services/lightbox/plugin/MangaControllerPlugin.jsx
@@ -0,0 +1,13 @@
+import { createModule } from "yet-another-react-lightbox";
+import { MangaController } from "../module/mangaController/mangaController";
+
+const MangaControllerModule = createModule(
+  "MangaControllerModule",
+  MangaController
+);
+
+function MangaControllerPlugin({ addModule, MangaControllerProps }) {
+  addModule(MangaControllerModule, { MangaControllerProps });
+}
+
+export default MangaControllerPlugin;
