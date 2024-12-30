84f4230428476ff61f7c21720bf7ba3d334bcb71
realrnvr <realrnvr@gmail.com>
2024-12-30 16:25:20 +0530

    code abstraction + improvements

--- client/src/pages/viewer/Viewer.jsx
+++ client/src/pages/viewer/Viewer.jsx
@@ -1,136 +1,14 @@
-import { useMemo, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { axiosInstance } from "../../services/api/axios";
-import { useParams } from "react-router-dom";
-import Lightbox from "yet-another-react-lightbox";
-
-// plugins
-import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
-import Zoom from "yet-another-react-lightbox/plugins/zoom";
-import Captions from "yet-another-react-lightbox/plugins/captions";
-import Download from "yet-another-react-lightbox/plugins/download";
-import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
-import Counter from "yet-another-react-lightbox/plugins/counter";
-import Inline from "yet-another-react-lightbox/plugins/inline";
-import { MangaControllerPlugin } from "../../plugin/MangaControllerPlugin";
-
-// css
-import "yet-another-react-lightbox/styles.css";
-import "yet-another-react-lightbox/plugins/captions.css";
-import "yet-another-react-lightbox/plugins/counter.css";
 import "./viewer.css";
-
-const CHUNK_SIZE = 100;
+import { useParams } from "react-router-dom";
+import LightBoxBase from "../../services/lightbox/LightBoxBase";
 
 const Viewer = () => {
   const { mangaId } = useParams();
-  // state
-  const [offset, setOffset] = useState(0);
-  const [quality, setQuality] = useState("data");
-  const [chapterCount, setChapterCount] = useState(0);
-
-  // chapter data
-  const { data: chapter } = useQuery({
-    queryKey: ["chapter", { mangaId, CHUNK_SIZE, offset }],
-    queryFn: () =>
-      axiosInstance.get(
-        `/api/v1/manga/chapters/${mangaId}?limit=${CHUNK_SIZE}&offset=${offset}`
-      ),
-  });
-
-  // data extraction
-  const chapters = chapter?.data?.data;
-  const totalChapters = chapter?.data?.total;
-  const chapterBound = chapter?.data?.chapterBound;
-
-  const chapterId = chapters && chapters[chapterCount]?.id;
-  const currChapter = chapters && chapters[chapterCount]?.attributes?.chapter;
-  const currVolume = chapters && chapters[chapterCount]?.attributes?.volume;
-
-  // chapter images
-  const { data, isLoading } = useQuery({
-    queryKey: ["chapter-image", { chapterId, quality }],
-    queryFn: () =>
-      axiosInstance.get(`/api/v1/manga/chapter-image/${chapterId}`, {
-        params: {
-          quality: quality,
-        },
-      }),
-    enabled: !!chapterId,
-  });
-
-  // image array
-  const slides = isLoading ? [] : data?.data?.data;
-
-  // memo chunk val
-  const hasPrevChunk = useMemo(() => offset > 0, [offset]);
-  const hasNextChunk = useMemo(
-    () => offset + CHUNK_SIZE < totalChapters,
-    [offset, totalChapters]
-  );
-
-  // handle chunk
-  const handlePrevChunk = () => {
-    if (hasPrevChunk) {
-      setOffset((prev) => prev - CHUNK_SIZE);
-      setChapterCount(0);
-    }
-  };
-
-  const handleNextChunk = () => {
-    if (hasNextChunk) {
-      setOffset((prev) => prev + CHUNK_SIZE);
-      setChapterCount(0);
-    }
-  };
 
   return (
-    <>
-      <div className="viewer">
-        <Lightbox
-          plugins={[
-            Inline,
-            Fullscreen,
-            Zoom,
-            Captions,
-            Download,
-            Slideshow,
-            Counter,
-            MangaControllerPlugin,
-          ]}
-          open={true}
-          slides={slides}
-          zoom={{
-            maxZoomPixelRatio: 2,
-            zoomInMultiplier: 2,
-            doubleClickMaxStops: 2,
-            keyboardMoveDistance: 100,
-          }}
-          carousel={{ finite: true }}
-          animation={{
-            zoom: 500,
-          }}
-          MangaControllerProps={{
-            chapterCount,
-            setChapterCount,
-            isLoading,
-            currChapter,
-            currVolume,
-            chapters,
-            nav: {
-              hasPrevChunk,
-              handlePrevChunk,
-              hasNextChunk,
-              handleNextChunk,
-              totalChapters,
-              chapterBound,
-            },
-            quality,
-            setQuality,
-          }}
-        />
-      </div>
-    </>
+    <article className="viewer">
+      <LightBoxBase mangaId={mangaId} />
+    </article>
   );
 };
 
