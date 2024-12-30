e295c5e40b91e1cd9bd7d796207a13dc744c00b0
realrnvr <realrnvr@gmail.com>
2024-12-30 16:24:18 +0530

    Base provider of lightbox

---
+++ client/src/services/lightbox/LightBoxBase.jsx
@@ -0,0 +1,37 @@
+// css
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/counter.css";
+// hooks
+import { useChapter } from "../../hooks/useChapter";
+// config
+import { animation, carousel, plugins, zoom } from "./config";
+// lightbox
+import Lightbox from "yet-another-react-lightbox";
+// other
+import PropTypes from "prop-types";
+
+const LightBoxBase = ({ mangaId }) => {
+  const { slides, isLoading, chapter, nav } = useChapter(mangaId);
+
+  return (
+    <Lightbox
+      plugins={plugins}
+      slides={slides}
+      zoom={zoom}
+      carousel={carousel}
+      animation={animation}
+      MangaControllerProps={{
+        chapter,
+        nav,
+        isLoading,
+      }}
+    />
+  );
+};
+
+LightBoxBase.propTypes = {
+  mangaId: PropTypes.string.isRequired,
+};
+
+export default LightBoxBase;
