f621c1a76993dd3922f73d4d82035e160bf61834
realrnvr <realrnvr@gmail.com>
2024-12-31 19:05:35 +0530

    lazy load in to its custom hook no abs

--- client/src/hooks/useLazyLoadEffect.js
+++ client/src/hooks/useLazyLoadEffect.js
@@ -1,11 +1,28 @@
 import { useEffect } from "react";
-import { lazyLoading } from "../helper/lazyLoad";
 
 export function useLazyLoadEffect({ carouselRef, lzImgsRef }) {
   useEffect(() => {
-    const observer = lazyLoading({
-      rootEl: carouselRef.current,
-      lzImgs: lzImgsRef.current,
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            let img = entry.target;
+            img.src = img.dataset.src;
+            img.classList.remove("loading");
+            img.classList.add("loaded");
+            observer.unobserve(img);
+          }
+        });
+      },
+      {
+        root: carouselRef.current,
+        rootMargin: "100px",
+        threshold: 0.01,
+      }
+    );
+
+    lzImgsRef.current?.forEach((img) => {
+      observer.observe(img);
     });
 
     return () => {
