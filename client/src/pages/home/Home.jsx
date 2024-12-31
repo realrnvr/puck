2cc64412d6618062bfdaf11439e37f2e26a349e3
realrnvr <realrnvr@gmail.com>
2024-12-31 19:04:59 +0530

    commented code removed

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,14 +1,11 @@
 import "./home.css";
 import Hero from "../../components/ui/hero/Hero";
-// import Showcase from "../../components/ui/showcase/Showcase";
 import Community from "../../components/ui/community/Community";
 
 const Home = () => {
   return (
     <>
       <Hero />
-      {/* <Showcase title={"Watch Anime"} /> */}
-      {/* <Showcase title={"Read Manga"} /> */}
       <Community />
     </>
   );
