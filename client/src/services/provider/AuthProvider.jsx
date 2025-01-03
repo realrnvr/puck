52d820d05e417248ee6cfc542d0426ce68430a19
realrnvr <realrnvr@gmail.com>
2025-01-03 20:32:11 +0530

    auth changes : [temp]

--- client/src/services/provider/AuthProvider.jsx
+++ client/src/services/provider/AuthProvider.jsx
@@ -1,59 +1,43 @@
 import { useEffect, useLayoutEffect, useState } from "react";
 import { axiosInstance } from "../api/axios.js";
 import { AuthContext } from "../../hooks/useAuth.js";
-import { useMutation } from "@tanstack/react-query";
+import { useLocalStorage } from "../../hooks/useLocalStorage.js";
 import PropTypes from "prop-types";
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState();
+  const [token, setToken] = useLocalStorage("token", undefined);
+  const [isLoading, setIsLoading] = useState(false);
   console.log("token state", token);
 
-  const { mutate: meMutate } = useMutation({
-    mutationFn: () => axiosInstance.post("/api/v1/auth/me"),
-    onSuccess: (data) => {
-      if (data.data?.isAuthenticated) {
-        setToken(data.data?.accessToken);
-      } else {
-        setToken(null);
-      }
-    },
-    onError: () => {
-      setToken(null);
-    },
-  });
-
   useEffect(() => {
-    meMutate();
-  }, [meMutate]);
+    const fetchMe = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axiosInstance.post("/api/v1/auth/me");
+        setToken(response.data.accessToken);
+      } catch (error) {
+        setToken(null);
+        console.error("Error fetching user data:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-  const { mutateAsync: refreshAccessTokenMutate } = useMutation({
-    mutationFn: () => axiosInstance.get("/api/v1/auth/refreshToken"),
-    onSuccess: (data) => {
-      console.log("refresh");
-      setToken(data?.data?.accessToken);
-    },
-    onError: (error) => {
-      console.error("Error refreshing access token:", error);
-      setToken(null);
-      throw error;
-    },
-  });
+    fetchMe();
+  }, [setToken]);
 
   useLayoutEffect(() => {
-    const authInterceptor = axiosInstance.interceptors.request.use(
-      (config) => {
-        if (token) {
-          config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-      },
-      (error) => {
-        console.error("Request error:", error);
-        return Promise.reject(error);
-      }
-    );
+    const authInterceptor = axiosInstance.interceptors.request.use((config) => {
+      config.headers.Authorization =
+        !config._retry && token
+          ? `Bearer ${token}`
+          : config.headers.Authorization;
+      return config;
+    });
 
-    return () => axiosInstance.interceptors.request.eject(authInterceptor);
+    return () => {
+      axiosInstance.interceptors.request.eject(authInterceptor);
+    };
   }, [token]);
 
   useLayoutEffect(() => {
@@ -61,30 +45,45 @@ export const AuthProvider = ({ children }) => {
       (response) => response,
       async (error) => {
         const originalRequest = error.config;
-        if (error.response?.status === 403 && !originalRequest._retry) {
+
+        if (
+          error.response?.status === 403 &&
+          error.response.data.message === "Forbidden" &&
+          !originalRequest._retry
+        ) {
           originalRequest._retry = true;
+
           try {
-            const newToken = await refreshAccessTokenMutate();
-            originalRequest.headers.Authorization = `Bearer ${newToken}`;
+            const response = await axiosInstance.get(
+              "/api/v1/auth/refreshToken"
+            );
+            setToken(response.data.accessToken);
+
+            originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
             return axiosInstance(originalRequest);
           } catch (refreshError) {
-            console.error("Failed to refresh token:", refreshError);
+            setToken(null);
+            console.error("Refresh token failed:", refreshError);
             return Promise.reject(refreshError);
           }
         }
+
+        console.error("API request failed:", error);
         return Promise.reject(error);
       }
     );
 
-    return () => axiosInstance.interceptors.response.eject(refreshInterceptor);
-  }, [refreshAccessTokenMutate]);
+    return () => {
+      axiosInstance.interceptors.response.eject(refreshInterceptor);
+    };
+  }, [setToken]);
 
   AuthProvider.propTypes = {
     children: PropTypes.node.isRequired,
   };
 
   return (
-    <AuthContext.Provider value={{ token, setToken }}>
+    <AuthContext.Provider value={{ token, setToken, isLoading }}>
       {children}
     </AuthContext.Provider>
   );
