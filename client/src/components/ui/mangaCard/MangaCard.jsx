ed2f9ff1418ab6439b0979eaa30697daeadc3c22
realrnvr <realrnvr@gmail.com>
2025-01-03 20:28:41 +0530

    fav btn addition

--- client/src/components/ui/mangaCard/MangaCard.jsx
+++ client/src/components/ui/mangaCard/MangaCard.jsx
@@ -2,15 +2,33 @@ import "./manga-card.css";
 import { memo } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import FavBtn from "../favBtn/FavBtn";
 
 const MangaCard = ({ img = "/1px.webp", title, mangaId, authorId }) => {
+  const mangaData = {
+    mangaTitle: title,
+    mangaId: mangaId,
+    authorId: authorId,
+    coverUrl: img,
+  };
+
   return (
-    <Link to={`/manga/${mangaId}/${authorId}`} className="manga-card__wrapper">
-      <figure className="manga-card">
-        <img className="manga-card__img" src={`${img}`} alt="manga" />
-        <figcaption className="manga-card__title">{title}</figcaption>
-      </figure>
-    </Link>
+    <div className="manga-card__container">
+      <FavBtn
+        mangaId={mangaId}
+        mangaData={mangaData}
+        className={"manga-card__btn"}
+      />
+      <Link
+        to={`/manga/${mangaId}/${authorId}`}
+        className="manga-card__wrapper"
+      >
+        <figure className="manga-card">
+          <img className="manga-card__img" src={`${img}`} alt="manga" />
+          <figcaption className="manga-card__title">{title}</figcaption>
+        </figure>
+      </Link>
+    </div>
   );
 };
 
