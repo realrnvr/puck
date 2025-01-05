4f298daa367b87b71bd5e7933e25b2ef8600687e
realrnvr <realrnvr@gmail.com>
2025-01-06 00:46:58 +0530

    mangas controller addition

--- server/controller/manga.js
+++ server/controller/manga.js
@@ -2,9 +2,39 @@ import { StatusCodes } from "http-status-codes";
 import { redisClient } from "../config/redisClient.js";
 import { BadRequestError } from "../errors/badRequestError.js";
 import { NotFoundError } from "../errors/notFoundError.js";
+import Manga from "../models/manga.js";
 import axios from "axios";
 
 const BASE_URL = "https://api.mangadex.org";
+
+export const mangas = async (req, res) => {
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const cacheKey = `mangas:${page}:${limit}`;
+
+  const cachedManga = await redisClient.get(cacheKey);
+  if (cachedManga) {
+    console.log("Manga cache hit");
+    return res.status(StatusCodes.OK).json(JSON.parse(cachedManga));
+  }
+
+  const mangaData = await Manga.find().skip(skip).limit(limit);
+  const totalManga = await Manga.countDocuments();
+
+  const numOfPages = Math.ceil(totalManga / limit);
+
+  const responseData = {
+    manga: mangaData,
+    totalManga,
+    numOfPages,
+  };
+
+  await redisClient.setEx(cacheKey, 3600, JSON.stringify(responseData));
+
+  res.status(StatusCodes.OK).json(responseData);
+};
 
 export const statics = async (req, res) => {
   const { mangaId } = req.params;
