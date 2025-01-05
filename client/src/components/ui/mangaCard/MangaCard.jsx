745d49cf9cfd8c581672c3cbc4a77813d521f6ab
realrnvr <realrnvr@gmail.com>
2025-01-06 00:45:21 +0530

    1px to t-1px

--- client/src/components/ui/mangaCard/MangaCard.jsx
+++ client/src/components/ui/mangaCard/MangaCard.jsx
@@ -4,7 +4,16 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import FavBtn from "../favBtn/FavBtn";
 
-const MangaCard = ({ img = "/1px.webp", title, mangaId, authorId }) => {
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const MangaCard = ({
+  img = "/t-1px.webp",
+  title,
+  mangaId,
+  authorId,
+  isLoading,
+}) => {
   const mangaData = {
     mangaTitle: title,
     mangaId: mangaId,
@@ -13,22 +22,61 @@ const MangaCard = ({ img = "/1px.webp", title, mangaId, authorId }) => {
   };
 
   return (
-    <div className="manga-card__container">
-      <FavBtn
-        mangaId={mangaId}
-        mangaData={mangaData}
-        className={"manga-card__btn"}
-      />
-      <Link
-        to={`/manga/${mangaId}/${authorId}`}
-        className="manga-card__wrapper"
-      >
-        <figure className="manga-card">
-          <img className="manga-card__img" src={`${img}`} alt="manga" />
-          <figcaption className="manga-card__title">{title}</figcaption>
-        </figure>
-      </Link>
-    </div>
+    <>
+      {!isLoading ? (
+        <div className="manga-card__container">
+          <FavBtn
+            mangaId={mangaId}
+            mangaData={mangaData}
+            className={"manga-card__btn"}
+          />
+          <Link
+            to={`/manga/${mangaId}/${authorId}`}
+            className="manga-card__wrapper"
+          >
+            <figure className="manga-card">
+              <img className="manga-card__img" src={`${img}`} alt="manga" />
+              <figcaption className="manga-card__title">{title}</figcaption>
+            </figure>
+          </Link>
+        </div>
+      ) : (
+        <div className="manga-card__skeleton-container">
+          <div className="manga-card__skeleton-btn">
+            <Skeleton
+              height={"100%"}
+              baseColor="#202020"
+              highlightColor="#444"
+            />
+          </div>
+          <figure className="manga-card__skeleton">
+            <div className="manga-card__skeleton-img">
+              <Skeleton
+                width={"100%"}
+                height={"100%"}
+                baseColor="#202020"
+                highlightColor="#444"
+              />
+            </div>
+            <img
+              className="manga-card__img"
+              style={{ visibility: "hidden" }}
+              src="/1px.webp"
+            />
+            <div className="manga-card__caption">
+              <h2 className="manga-card__title">
+                <Skeleton
+                  className="manga-card__title-skeleton"
+                  baseColor="#202020"
+                  highlightColor="#444"
+                  direction="rtl"
+                />
+              </h2>
+            </div>
+          </figure>
+        </div>
+      )}
+    </>
   );
 };
 
@@ -37,6 +85,7 @@ MangaCard.propTypes = {
   title: PropTypes.string.isRequired,
   mangaId: PropTypes.string.isRequired,
   authorId: PropTypes.string.isRequired,
+  isLoading: PropTypes.bool,
 };
 
 export default memo(MangaCard);
