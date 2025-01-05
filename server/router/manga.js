5c2bfdb8cce8925335c06a2222440d1bb541f851
realrnvr <realrnvr@gmail.com>
2025-01-06 00:47:46 +0530

    GET ("/mangas") route addition

--- server/router/manga.js
+++ server/router/manga.js
@@ -4,11 +4,13 @@ import {
   chapterImage,
   chapters,
   cover,
+  mangas,
   statics,
 } from "../controller/manga.js";
 
 const router = Router();
 
+router.route("/mangas").get(mangas);
 router.route("/statics/:mangaId").get(statics);
 router.route("/author/:authorId").get(author);
 router.route("/cover/:mangaId").get(cover);
