1e91317b0321549d9079cd278ff9dfba1091dadb
realrnvr <realrnvr@gmail.com>
2025-01-07 01:01:19 +0530

    cursor query addition

--- server/controller/manga.js
+++ server/controller/manga.js
@@ -8,27 +8,29 @@ import axios from "axios";
 const BASE_URL = "https://api.mangadex.org";
 
 export const mangas = async (req, res) => {
-  const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
-  const skip = (page - 1) * limit;
+  const cursor = req.query.cursor;
 
-  const cacheKey = `mangas:${page}:${limit}`;
+  let query = {};
+  if (cursor) {
+    query = { _id: { $gt: cursor } };
+  }
 
+  const cacheKey = `mangas:${cursor || "start"}:${limit}`;
   const cachedManga = await redisClient.get(cacheKey);
   if (cachedManga) {
     console.log("Manga cache hit");
     return res.status(StatusCodes.OK).json(JSON.parse(cachedManga));
   }
 
-  const mangaData = await Manga.find().skip(skip).limit(limit);
-  const totalManga = await Manga.countDocuments();
+  const mangaData = await Manga.find(query).sort({ _id: 1 }).limit(limit);
 
-  const numOfPages = Math.ceil(totalManga / limit);
+  const nextCursor =
+    mangaData.length === limit ? mangaData[mangaData.length - 1]._id : null;
 
   const responseData = {
     manga: mangaData,
-    totalManga,
-    numOfPages,
+    nextCursor,
   };
 
   await redisClient.setEx(cacheKey, 3600, JSON.stringify(responseData));
