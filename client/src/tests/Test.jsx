4d1b03ceb70c60a7fdecf4c3233dff2d1631af22
realrnvr <realrnvr@gmail.com>
2025-01-08 23:53:46 +0530

    search test

--- client/src/tests/Test.jsx
+++ client/src/tests/Test.jsx
@@ -1,27 +1,8 @@
 import "./test.css";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import Search from "../components/Search";
 
 const Test = () => {
-  return (
-    <SkeletonTheme
-      baseColor="#202020"
-      highlightColor="#444"
-      height={"100%"}
-      direction="rtl"
-      // enableAnimation={false}
-      // customHighlightBackground="linear-gradient(90deg, var(--base-color) 50%, var(--highlight-color) 80%, var(--base-color) 60%)"
-    >
-      <div className="test">
-        <div className="box">
-          <Skeleton containerClassName="flex-1" />
-        </div>
-        <div className="circle">
-          <Skeleton circle={true} />
-        </div>
-      </div>
-    </SkeletonTheme>
-  );
+  return <Search />;
 };
 
 export default Test;
