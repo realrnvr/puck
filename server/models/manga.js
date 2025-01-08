4ff2c09b6fe8a131742ab9853062d7091a45822c
realrnvr <realrnvr@gmail.com>
2025-01-08 23:54:34 +0530

    model changes : [temp]

--- server/models/manga.js
+++ server/models/manga.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const MangaSchema = new mongoose.Schema({
   img: { type: String },
-  title: { type: String },
+  title: { type: String, required: true },
   mangaId: { type: String, unique: true },
   authorId: { type: String },
 });
