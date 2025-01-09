75d7d48d480e8da5549655ce219087908f134e4b
realrnvr <realrnvr@gmail.com>
2025-01-09 20:26:37 +0530

    search comp. + redundant div removed

--- client/src/components/ui/header/Header.jsx
+++ client/src/components/ui/header/Header.jsx
@@ -159,9 +159,7 @@ const Header = () => {
               {/* <Link className="header__link" to="/account">
                   My Account
                 </Link> */}
-              <div>
-                <Search />
-              </div>
+              <Search />
             </div>
             <div>
               <ul className="header__pri-ul header__pri-ul--flex-d">
@@ -357,28 +355,7 @@ const Header = () => {
               </Link>
             </li>
           </ul>
-          <form className="header__form" aria-label="Search Manga and Anime">
-            <input
-              type="text"
-              name="search"
-              placeholder="Search"
-              className="header__input"
-            />
-            <button
-              className="header__btn header__search-btn"
-              aria-label="Submit"
-            >
-              <svg
-                viewBox="0 0 16 16"
-                className="header__icon header__icon--width header__icon--color"
-              >
-                <path
-                  d="M6.38 0A6.3 6.3 0 000 6.23a6.3 6.3 0 006.38 6.24c1.26 0 2.43-.36 3.41-.97L14.4 16l1.6-1.56-4.55-4.43a6.1 6.1 0 001.31-3.78A6.3 6.3 0 006.38 0zm0 1.47c2.7 0 4.88 2.12 4.88 4.76A4.82 4.82 0 016.38 11 4.82 4.82 0 011.5 6.23a4.82 4.82 0 014.88-4.76z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </button>
-          </form>
+          <Search />
         </nav>
         <nav
           className="header__mobile-sec-nav"
