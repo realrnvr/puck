58692fb998cac186aed03e80b816deceab553510
realrnvr <realrnvr@gmail.com>
2025-01-10 00:55:57 +0530

    fetch random-manga + static data removed

--- client/src/pages/read/Read.jsx
+++ client/src/pages/read/Read.jsx
@@ -1,34 +1,18 @@
 import "./read.css";
 import { Link } from "react-router-dom";
+import { axiosInstance } from "../../services/api/axios";
+import { useQuery } from "@tanstack/react-query";
 import MangaCard from "../../components/ui/mangaCard/MangaCard";
+import MangaCardSkeleton from "../../utils/skeletons/MangaCard/MangaCardSkeleton";
+
+const LIMIT = 4;
 
 const Read = () => {
-  const caseManga = [
-    {
-      img: "/berserk-manga-cover.jpg",
-      title: "Berserk",
-      mangaId: "801513ba-a712-498c-8f57-cae55b38cc92",
-      authorId: "5863578d-4e4f-4b57-b64d-1dd45a893cb0",
-    },
-    {
-      img: "/vaga-bond-manga-cover.jpg",
-      title: "Vaga Bond",
-      mangaId: "d1a9fdeb-f713-407f-960c-8326b586e6fd",
-      authorId: "0b59098f-13c0-41cb-a110-9cbcec72dc0c",
-    },
-    {
-      img: "/vinland-saga-manga-cover.jpg",
-      title: "Vinland Saga",
-      mangaId: "5d1fc77e-706a-4fc5-bea8-486c9be0145d",
-      authorId: "f5d4fca1-d573-4383-af08-c06b0794ba4e",
-    },
-    {
-      img: "/monster-manga-cover.jpg",
-      title: "Monster",
-      mangaId: "d9e30523-9d65-469e-92a2-302995770950",
-      authorId: "508631f5-09de-4ae1-87ed-4b6179254ca1",
-    },
-  ];
+  const { data, isPending } = useQuery({
+    queryKey: ["random-manga"],
+    queryFn: () =>
+      axiosInstance.get(`/api/v1/manga/random-manga?limit=${LIMIT}`),
+  });
 
   return (
     <>
@@ -53,7 +37,7 @@ const Read = () => {
               </Link>
             </div>
             <div className="read__manga-container">
-              {caseManga.map((val, idx) => {
+              {data?.data?.manga.map((val, idx) => {
                 return (
                   <MangaCard
                     key={idx}
@@ -64,6 +48,7 @@ const Read = () => {
                   />
                 );
               })}
+              {isPending ? <MangaCardSkeleton count={LIMIT} /> : null}
             </div>
             <Link to="/mangas" className="read__link sm-screen">
               See all manga series
