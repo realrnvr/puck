21a91b561eda039605dc67f0db1edbf2aad81977
realrnvr <realrnvr@gmail.com>
2025-01-16 23:35:57 +0530

    change password input data

---
+++ client/src/assets/data/changePasswordData.js
@@ -0,0 +1,26 @@
+export const changePassword = [
+  {
+    label: "Old Password",
+    name: "oldPassword",
+    type: "password",
+    serverErr: "user not found.",
+    defaultMsg: "At least 8 characters",
+    toggle: true,
+  },
+  {
+    label: "New Password",
+    name: "newPassword",
+    type: "password",
+    serverErr: "user not found.",
+    defaultMsg: "At least 8 characters",
+    toggle: true,
+  },
+  {
+    label: "Confirm New Password",
+    name: "confirmNewPassword",
+    type: "password",
+    serverErr: "user not found.",
+    defaultMsg: "Valid",
+    toggle: true,
+  },
+];
