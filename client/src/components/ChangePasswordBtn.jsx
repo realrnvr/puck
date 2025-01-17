2e897e53c21c897332634ec47884eb1a38360bb6
realrnvr <realrnvr@gmail.com>
2025-01-17 19:47:48 +0530

    Change password btn

---
+++ client/src/components/ChangePasswordBtn.jsx
@@ -0,0 +1,47 @@
+import toast from "react-hot-toast";
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+import NavigationToast from "./auth/navigationToast/NavigationToast";
+
+const ChangePasswordBtn = ({ user, isPending }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (user.type === "google") {
+      toast(
+        <NavigationToast
+          message={"create password:"}
+          btnText={"here"}
+          email={user?.email}
+          navigate={navigate}
+          to={"/login/loginGoogleAuth"}
+          toastId={"create-password-toast"}
+          path={"log-mail"}
+        />,
+        {
+          duration: 10000,
+          id: "create-password-toast",
+        }
+      );
+    } else {
+      navigate("/change-password");
+    }
+  };
+  return (
+    <button
+      type="button"
+      className="account__change-btn"
+      onClick={handleClick}
+      disabled={isPending}
+    >
+      Change Password
+    </button>
+  );
+};
+
+ChangePasswordBtn.propTypes = {
+  user: PropTypes.object,
+  isPending: PropTypes.bool,
+};
+
+export default ChangePasswordBtn;
