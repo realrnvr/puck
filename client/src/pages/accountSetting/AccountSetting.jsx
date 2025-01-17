4e021471bac38a83cd1c66c8f113b7dd09c8d1f2
realrnvr <realrnvr@gmail.com>
2025-01-17 19:51:54 +0530

    UI changes

--- client/src/pages/accountSetting/AccountSetting.jsx
+++ client/src/pages/accountSetting/AccountSetting.jsx
@@ -7,9 +7,11 @@ import { axiosInstance } from "../../services/api/axios";
 import { useAuth } from "../../hooks/useAuth";
 import { hasErrors } from "../../helper/hasErrors";
 import Input from "../../components/ui/input/Input";
+import ChangePasswordBtn from "../../components/ChangePasswordBtn";
+import Skeleton from "react-loading-skeleton";
 
 const AccountSetting = () => {
-  const { user } = useAuth();
+  const { user, isPending } = useAuth();
   const queryClient = useQueryClient();
 
   const {
@@ -31,7 +33,7 @@ const AccountSetting = () => {
     return axiosInstance.patch("/api/v1/client/update-username", data);
   };
 
-  const { mutate, isPending } = useMutation({
+  const { mutate, isPending: mutateIsPending } = useMutation({
     mutationFn: updateUsername,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["user"] });
@@ -50,32 +52,57 @@ const AccountSetting = () => {
 
   return (
     <section className="setting | container">
-      <h2 className="setting__title">Edit Account Settings</h2>
-      <div className="setting__option">
-        <h3 className="setting__sec-title">Profile</h3>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Input
-            register={register}
-            watch={watch}
-            errors={errors}
-            formData={{
-              label: "Username",
-              name: "newUsername",
-              type: "text",
-              serverErr: "username already exists.",
-              defaultMsg: "At least 4 characters",
-              toggle: false,
-            }}
-            placeholder={user?.username}
-          />
-          <button
-            type="submit"
-            className="setting__btn"
-            disabled={isPending || !isValid || hasErrors(errors)}
-          >
-            edit
-          </button>
-        </form>
+      <div>
+        <h2 className="setting__title">Edit Account Settings</h2>
+        <div className="setting__option">
+          <h3 className="setting__sec-title">Profile</h3>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              register={register}
+              watch={watch}
+              errors={errors}
+              formData={{
+                label: "Username",
+                name: "newUsername",
+                type: "text",
+                serverErr: "username already exists.",
+                defaultMsg: "At least 4 characters",
+                toggle: false,
+              }}
+              placeholder={user?.username}
+              inputWrapperClassName={"setting__input"}
+            />
+            <button
+              type="submit"
+              className="setting__btn"
+              disabled={mutateIsPending || !isValid || hasErrors(errors)}
+            >
+              edit
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="setting__bottom-container">
+        <h2 className="setting__sec-title">Account Information</h2>
+        <div className="setting__opt-container">
+          <span className="setting__label">E-mail:</span>
+          <span>
+            {isPending ? (
+              <Skeleton
+                baseColor="#202020"
+                highlightColor="#444"
+                height={"100%"}
+                width={"150px"}
+              />
+            ) : (
+              user?.email
+            )}
+          </span>
+        </div>
+        <div className="setting__opt-container">
+          <span className="setting__label">Password:</span>
+          <ChangePasswordBtn user={user} isPending={isPending} />
+        </div>
       </div>
     </section>
   );
