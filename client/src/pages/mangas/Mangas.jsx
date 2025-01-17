cda11a02633db36a0bc61e5370b7956ca29e0262
realrnvr <realrnvr@gmail.com>
2025-01-17 23:42:43 +0530

    GoBackBtn addition

--- client/src/pages/mangas/Mangas.jsx
+++ client/src/pages/mangas/Mangas.jsx
@@ -1,33 +1,14 @@
 import "./mangas.css";
 import MangaContainer from "../../components/mangaContainer";
-import { Link } from "react-router-dom";
-import { caseManga } from "../../utils/MangaData";
+import GoBackBtn from "../../components/ui/goBackBtn/GoBackBtn";
 
 const Mangas = () => {
   return (
     <section className="mangas | container">
-      <Link to="/read">
-        <div className="mangas__back">
-          <svg
-            className="mangas__arrow-svg"
-            viewBox="0 0 512 512"
-            fill="currentColor"
-          >
-            <path
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={48}
-              d="M244 400L100 256l144-144M120 256h292"
-            />
-          </svg>
-          <span className="mangas__span-text">Read</span>
-        </div>
-      </Link>
+      <GoBackBtn />
       <div className="mangas__content">
         <h2 className="mangas__title">Read The Hit Manga Series!</h2>
-        <MangaContainer caseManga={caseManga} />
+        <MangaContainer />
       </div>
     </section>
   );
