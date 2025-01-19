ce5e08b7df99098b2374b1e9575930a8167c08b5
realrnvr <realrnvr@gmail.com>
2025-01-19 21:25:41 +0530

    manga data hook

---
+++ client/src/hooks/useMangaData.js
@@ -0,0 +1,50 @@
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "../services/api/axios";
+import { useMemo } from "react";
+
+export const useMangaData = ({ mangaId, authorId }) => {
+  const { data: statics, isPending: isStatics } = useQuery({
+    queryKey: ["static", { mangaId }],
+    queryFn: () => axiosInstance.get(`/api/v1/manga/statics/${mangaId}`),
+  });
+
+  const { data: authorData, isPending: isAuthor } = useQuery({
+    queryKey: ["author", { authorId }],
+    queryFn: () => axiosInstance.get(`/api/v1/manga/author/${authorId}`),
+  });
+
+  const { data: coverImg, isPending: isCover } = useQuery({
+    queryKey: ["coverImg", { mangaId }],
+    queryFn: () => axiosInstance.get(`/api/v1/manga/cover/${mangaId}`),
+  });
+
+  const manga = statics?.data?.data?.attributes?.description?.en;
+  const author = authorData?.data?.data?.attributes?.biography?.en;
+
+  const mangaData = useMemo(
+    () => ({
+      mangaTitle: statics?.data?.data?.attributes?.title?.en,
+      mangaId: mangaId,
+      authorId: authorId,
+      coverUrl: coverImg?.data?.coverImgUrl,
+    }),
+    [
+      statics?.data?.data?.attributes?.title?.en,
+      mangaId,
+      authorId,
+      coverImg?.data?.coverImgUrl,
+    ]
+  );
+
+  return {
+    statics,
+    isStatics,
+    authorData,
+    isAuthor,
+    coverImg,
+    isCover,
+    manga,
+    author,
+    mangaData,
+  };
+};
