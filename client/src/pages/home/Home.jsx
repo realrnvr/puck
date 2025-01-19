4f69442c7477130075a0c600177a0ae0d0545157
realrnvr <realrnvr@gmail.com>
2025-01-19 21:25:14 +0530

    case manga addition

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import "./home.css";
 import Hero from "../../components/ui/hero/Hero";
 import Community from "../../components/ui/community/Community";
+import CaseManga from "../../components/ui/caseManga/CaseManga";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <CaseManga />
       <Community />
     </>
   );
