1a9207a4b6a9d68ab56d9acb152c0ab5ebfd94f0
realrnvr <realrnvr@gmail.com>
2025-01-19 21:24:30 +0530

    slider config

---
+++ client/src/services/sliderbox/config.js
@@ -0,0 +1,27 @@
+import Captions from "yet-another-react-lightbox/plugins/captions";
+import Inline from "yet-another-react-lightbox/plugins/inline";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import SliderPanelPlugin from "./plugin/SliderPanelPlugin";
+
+export const plugins = [
+  Thumbnails,
+  Captions,
+  Inline,
+  Slideshow,
+  SliderPanelPlugin,
+];
+
+export const slideshow = {
+  autoplay: true,
+  delay: 5000,
+};
+
+export const thumbnails = {
+  width: 40,
+  height: 40,
+};
+
+export const carousel = {
+  preload: 5,
+};
