de3a483e3b85fa6e0e0d9286041ea98619fe4bf7
realrnvr <realrnvr@gmail.com>
2025-01-19 21:24:21 +0530

    base for slider

---
+++ client/src/services/sliderbox/SliderBoxBase.jsx
@@ -0,0 +1,35 @@
+// css
+import "./slider-box-base.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+// plugins
+import { carousel, plugins, slideshow, thumbnails } from "./config";
+// skeleton
+import SliderBoxSkeleton from "./skeleton/SliderBoxSkeleton";
+// lightbox
+import Lightbox from "yet-another-react-lightbox";
+// hooks
+import { useSliderManga } from "../../hooks/useSliderManga";
+
+const SliderBoxBase = () => {
+  const { slides, isCoversLoading, isMangaLoading } = useSliderManga();
+
+  if (isMangaLoading || isCoversLoading) {
+    return <SliderBoxSkeleton />;
+  }
+
+  return (
+    <div className="slider">
+      <Lightbox
+        plugins={plugins}
+        slideshow={slideshow}
+        thumbnails={thumbnails}
+        carousel={carousel}
+        slides={slides}
+      />
+    </div>
+  );
+};
+
+export default SliderBoxBase;
