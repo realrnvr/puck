a00184ddc2978979931f14a4e1e106a99f632469
realrnvr <realrnvr@gmail.com>
2025-01-20 19:20:46 +0530

    Community page

---
+++ client/src/pages/community/Community.jsx
@@ -0,0 +1,77 @@
+import "./community.css";
+
+const Community = () => {
+  return (
+    <>
+      <img
+        src="/pattern-6.svg"
+        className="read__bg-img"
+        alt="pattern"
+        aria-hidden="true"
+      />
+      <article className="read">
+        <div className="read__top-wrapper | container">
+          <div className="read__intro-container">
+            <h2 className="read__title read__title--fs">Community</h2>
+            <p className="read__description">
+              Connect with fellow manga and anime enthusiasts! Share your
+              thoughts, discuss favorites, and discover recommendations in a
+              vibrant and welcoming space.
+            </p>
+          </div>
+        </div>
+        <div className="read__manga-wrapper">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio velit
+            minus unde sed, similique laboriosam iure labore deleniti deserunt
+            nihil esse sint eaque delectus rerum vero, et quos libero? Fugit
+            provident ex earum, quaerat architecto quasi eligendi fugiat enim,
+            tenetur modi sint est accusamus doloribus nemo sapiente omnis
+            voluptates ipsam eius minus pariatur hic voluptas nobis? Vitae
+            architecto expedita excepturi amet necessitatibus fuga officiis?
+            Suscipit harum ea quas qui dolorum asperiores ut id rem cupiditate
+            odio sunt voluptatum accusantium autem minima non totam vero iusto,
+            quaerat porro natus molestias nisi impedit consequatur. Ex sint
+            distinctio adipisci id atque magnam, velit placeat voluptate
+            necessitatibus quam voluptates dignissimos aspernatur aliquid ad
+            cupiditate aperiam voluptatum libero consequuntur cum. Natus tempora
+            nulla incidunt enim excepturi beatae minima repudiandae explicabo
+            tempore ad impedit voluptatibus, dolorum eos veniam quis, veritatis
+            quasi quos, cum sint quas? Adipisci ipsam consequuntur eius harum
+            quaerat laboriosam voluptatem fugit placeat! Accusamus consequuntur
+            vero alias reiciendis est dolore rerum consequatur, eligendi cum
+            sunt ratione adipisci facilis repellendus rem ad necessitatibus hic
+            odit. Quasi aspernatur corrupti labore nisi, minus magnam odit aut
+            iure culpa cupiditate natus soluta alias eaque quas ratione autem
+            harum! Quas asperiores, iure adipisci alias assumenda voluptates,
+            explicabo fugit, consequuntur quasi aut porro accusantium placeat
+            ut. Omnis porro dolore, animi minus fugiat magni, eos iste sint unde
+            mollitia blanditiis voluptatum qui vitae fugit, enim aperiam. At
+            quisquam quibusdam illo repellendus? Aliquid sint, nemo suscipit
+            unde rerum harum ab ad quaerat alias consequuntur ex ducimus
+            accusamus perspiciatis quo corrupti amet voluptate cumque
+            praesentium explicabo eveniet. Aspernatur fuga magnam minus
+            blanditiis numquam molestiae incidunt veritatis non dicta temporibus
+            harum error neque ipsam ducimus doloribus, at quam nihil aliquam
+            sint qui! Id quo mollitia excepturi quis dolor minus perspiciatis
+            obcaecati optio omnis sed quas quae repellendus architecto ipsam
+            sapiente iste minima non odio reiciendis amet quisquam, officiis
+            vero, at nemo. Earum ipsa unde voluptas, libero alias atque repellat
+            expedita, animi itaque quasi totam tempore minima laborum pariatur
+            dolore harum perspiciatis, dolor iste corporis ea eaque quibusdam
+            odio et iusto. Quis repudiandae voluptates itaque ipsa ratione
+            laborum vitae, deserunt nisi laboriosam corporis totam eos quod
+            necessitatibus porro, incidunt at fuga error. Tempore adipisci
+            praesentium, a rem vitae illo amet omnis nam molestiae temporibus
+            sequi neque ab quidem exercitationem quisquam consequuntur
+            voluptatum, eius ad soluta quas dignissimos possimus assumenda.
+            Accusamus molestias, adipisci est aspernatur ullam animi odit et
+            recusandae ipsa modi culpa fuga excepturi libero.
+          </p>
+        </div>
+      </article>
+    </>
+  );
+};
+
+export default Community;
