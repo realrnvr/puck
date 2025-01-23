b7c1591b5a3bb7e6ea5f3fcec2bf4c6da875e53a
realrnvr <realrnvr@gmail.com>
2025-01-23 19:11:01 +0530

    when loading manga card img only image will show the loading skeleton and will not block the whole card

--- client/src/components/ui/mangaCard/MangaCard.jsx
+++ client/src/components/ui/mangaCard/MangaCard.jsx
@@ -3,9 +3,9 @@ import { memo } from "react";
 import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "../../../services/api/axios";
+import MangaCardImgSkeleton from "../../../utils/skeletons/mangaCardImg/MangaCardImgSkeleton";
 import PropTypes from "prop-types";
 import FavBtn from "../favBtn/FavBtn";
-import MangaCardSkeleton from "../../../utils/skeletons/MangaCard/MangaCardSkeleton";
 
 const MangaCard = ({ title, mangaId, authorId }) => {
   const { data, isLoading, isError } = useQuery({
@@ -23,32 +23,30 @@ const MangaCard = ({ title, mangaId, authorId }) => {
   };
 
   return (
-    <>
-      {!isLoading ? (
-        <div className="manga-card__container">
-          <FavBtn
-            mangaId={mangaId}
-            mangaData={mangaData}
-            className={"manga-card__btn"}
-          />
-          <Link
-            to={`/manga/${mangaId}/${authorId}`}
-            className="manga-card__wrapper"
-          >
-            <figure className="manga-card">
-              <img
-                className="manga-card__img"
-                src={isError ? "/1px.webp" : `${coverUrl}`}
-                alt="manga"
-              />
-              <figcaption className="manga-card__title">{title}</figcaption>
-            </figure>
-          </Link>
-        </div>
-      ) : (
-        <MangaCardSkeleton />
-      )}
-    </>
+    <div className="manga-card__container">
+      <FavBtn
+        mangaId={mangaId}
+        mangaData={mangaData}
+        className={"manga-card__btn"}
+      />
+      <Link
+        to={`/manga/${mangaId}/${authorId}`}
+        className="manga-card__wrapper"
+      >
+        <figure className="manga-card">
+          {isLoading ? (
+            <MangaCardImgSkeleton />
+          ) : (
+            <img
+              className="manga-card__img"
+              src={isError ? "/1px.webp" : `${coverUrl}`}
+              alt="manga"
+            />
+          )}
+          <figcaption className="manga-card__title">{title}</figcaption>
+        </figure>
+      </Link>
+    </div>
   );
 };
 
