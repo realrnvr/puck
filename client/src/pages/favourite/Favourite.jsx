cb57d62a593889a0ced6025dd650a042a9bddcd3
realrnvr <realrnvr@gmail.com>
2025-01-24 19:17:17 +0530

    cls improved

--- client/src/pages/favourite/Favourite.jsx
+++ client/src/pages/favourite/Favourite.jsx
@@ -5,6 +5,7 @@ import { Fragment } from "react";
 import { fetchFavourites } from "../../services/query/query";
 import MangaCard from "../../components/ui/mangaCard/MangaCard";
 import MangaCardSkeleton from "../../utils/skeletons/MangaCard/MangaCardSkeleton";
+import Skeleton from "react-loading-skeleton";
 
 const LIMIT = 6;
 
@@ -30,42 +31,43 @@ const Favourite = () => {
   return (
     <section className="favourite | container">
       <h2 className="favourite__title">Favourites</h2>
-      {!data?.pages?.[0].client.length && !isLoading && !isError ? (
-        <div className="favourite__null">
-          <h4 className="favourite__null-title">NOTHING!</h4>
-          <p className="favourite__null-description">
-            Your favourite list is empty!
-          </p>
-          <img className="favourite__null-img" src="/no-fav.webp" alt="" />
-        </div>
-      ) : (
-        <div className="mangas__container">
-          {data?.pages?.map((page, idx) => {
-            return (
-              <Fragment key={idx}>
-                {page?.client?.map((val, idx) => {
-                  return (
-                    <MangaCard
-                      key={idx}
-                      img={val?.coverUrl}
-                      title={val?.mangaTitle}
-                      mangaId={val?.mangaId}
-                      authorId={val?.authorId}
-                    />
-                  );
-                })}
-              </Fragment>
-            );
-          })}
-          {isFetching && !isFetchingNextPage && !data?.pages?.[0] ? (
-            <MangaCardSkeleton count={LIMIT} />
-          ) : null}
-        </div>
-      )}
-      {data?.pages?.[0].client.length ? (
+      <div
+        className={`mangas__container ${
+          data?.pages?.[0]?.client?.length === 0 || isError
+            ? "mangas__container--pd"
+            : null
+        }`}
+      >
+        {!isLoading && !isError
+          ? data?.pages?.map((page, idx) => {
+              return (
+                <Fragment key={idx}>
+                  {page?.client?.map((val, idx) => {
+                    return (
+                      <MangaCard
+                        key={idx}
+                        img={val?.coverUrl}
+                        title={val?.mangaTitle}
+                        mangaId={val?.mangaId}
+                        authorId={val?.authorId}
+                      />
+                    );
+                  })}
+                </Fragment>
+              );
+            })
+          : null}
+        {isFetching && !isFetchingNextPage && !data?.pages?.[0] ? (
+          <MangaCardSkeleton count={LIMIT} />
+        ) : null}
+        {!(data?.pageParams?.[0] === "") && !isFetching ? (
+          <MangaCardSkeleton count={LIMIT} />
+        ) : null}
+      </div>
+      {data?.pages?.[0].client.length && !isError ? (
         <div className="favourite__bottom-container">
           <button
-            onClick={() => fetchNextPage()}
+            onClick={fetchNextPage}
             disabled={!hasNextPage || isFetchingNextPage}
             className="favourite__load-more-btn"
           >
@@ -77,16 +79,34 @@ const Favourite = () => {
           </button>
         </div>
       ) : null}
-      {/* {!data?.pages?.[0].client.length && !isPending && !isError ? (
+      {!(data?.pageParams?.[0] === "") ? (
+        <div className="favourite__bottom-container">
+          <button className="favourite__load-more-btn favourite__load-more-btn--cs-bg">
+            <Skeleton
+              width={"100px"}
+              height={"100%"}
+              baseColor="#202020"
+              highlightColor="#444"
+            />
+          </button>
+        </div>
+      ) : null}
+      {data?.pages?.[0]?.client?.length === 0 ? (
         <div className="favourite__null">
           <h4 className="favourite__null-title">NOTHING!</h4>
           <p className="favourite__null-description">
             Your favourite list is empty!
           </p>
-          <img className="favourite__null-img" src="/no-fav.png" alt="" />
+          <img className="favourite__null-img" src="/no-fav.webp" alt="" />
         </div>
-      ) : null} */}
-      {isError ? <p className="favourite__alt">{error?.message}</p> : null}
+      ) : null}
+      {isError ? (
+        <div className="favourite__null">
+          <h4 className="favourite__null-title">ERROR &#58;&#40;</h4>
+          <p className="favourite__null-description">{error?.message}</p>
+          <img className="favourite__null-img" src="/no-fav.webp" alt="" />
+        </div>
+      ) : null}
     </section>
   );
 };
