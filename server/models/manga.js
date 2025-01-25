1d1310417cfa2ce9ae8af0f91ec6afa29e54932d
realrnvr <realrnvr@gmail.com>
2025-01-26 00:37:50 +0530

    required filters

--- server/models/manga.js
+++ server/models/manga.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
 const MangaSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  mangaId: { type: String, unique: true },
-  authorId: { type: String },
+  title: { type: String, required: [true, "please provide manga title"] },
+  mangaId: {
+    type: String,
+    required: [true, "please provide manga id"],
+    unique: true,
+  },
+  authorId: { type: String, required: [true, "please provide author id"] },
   description: { type: String },
 });
 
