7510c673f0ad52ad30b6bc78064d514a96aed3fa
realrnvr <realrnvr@gmail.com>
2025-01-26 00:39:57 +0530

    set cache header

---
+++ server/helper/setCacheHeaders.js
@@ -0,0 +1,10 @@
+export const setCacheHeaders = ({
+  res,
+  cacheStatus,
+  ttl,
+  cType = "application/json",
+}) => {
+  res.set("X-Cache", cacheStatus); // Cache hit/miss status
+  res.set("Cache-Control", `public, max-age=${ttl}`); // Dynamic cache expiry
+  res.set("Content-Type", cType); // Content type
+};
