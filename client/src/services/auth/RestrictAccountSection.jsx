0dcf6bc6dae8cf260c86c7ddedace7a1d8425358
realrnvr <realrnvr@gmail.com>
2025-01-26 16:58:34 +0530

    type + diff skeletons added

--- client/src/services/auth/RestrictAccountSection.jsx
+++ client/src/services/auth/RestrictAccountSection.jsx
@@ -1,9 +1,30 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 import PropTypes from "prop-types";
+import AccountSkeleton from "../../utils/skeletons/account/AccountSkeleton";
+import AccountSettingSkeleton from "../../utils/skeletons/accountSettingSkeleton/AccountSettingSkeleton";
+import ChangePasswordSkeleton from "../../utils/skeletons/changePassword/ChangePasswordSkeleton";
 
-const RestrictAccountSection = ({ children, to = "/login" }) => {
+const RestrictAccountSection = ({
+  children,
+  to = "/login",
+  type = "account",
+}) => {
   const auth = useAuth();
+
+  const skeleton = {
+    account: <AccountSkeleton />,
+    accountSetting: <AccountSettingSkeleton />,
+    changePassword: <ChangePasswordSkeleton />,
+  };
+
+  const getSkeleton = (type) => {
+    return skeleton[type];
+  };
+
+  if (auth.mutateTokenPending) {
+    return getSkeleton(type);
+  }
 
   if (!auth.token) {
     return <Navigate to={to} replace />;
@@ -15,6 +36,7 @@ const RestrictAccountSection = ({ children, to = "/login" }) => {
 RestrictAccountSection.propTypes = {
   children: PropTypes.node.isRequired,
   to: PropTypes.string,
+  type: PropTypes.oneOf(["account", "accountSetting", "changePassword"]),
 };
 
 export default RestrictAccountSection;
