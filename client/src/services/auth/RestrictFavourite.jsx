c9438f81f526933ecbe8c76884d286d4484971d5
realrnvr <realrnvr@gmail.com>
2025-01-26 16:58:22 +0530

    loading before check

--- client/src/services/auth/RestrictFavourite.jsx
+++ client/src/services/auth/RestrictFavourite.jsx
@@ -1,9 +1,14 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 import PropTypes from "prop-types";
+import FavouriteSkeleton from "../../utils/skeletons/favourite/FavouriteSkeleton";
 
 const RestrictFavourite = ({ children }) => {
   const auth = useAuth();
+
+  if (auth.mutateTokenPending) {
+    return <FavouriteSkeleton />;
+  }
 
   if (!auth.token) {
     return <Navigate to={"/signup"} replace />;
