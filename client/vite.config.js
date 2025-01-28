a5b511fd265b874d4758af6e3bba1e00c5d4b583
realrnvr <realrnvr@gmail.com>
2025-01-28 22:18:14 +0530

    tests

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import Inspect from "vite-plugin-inspect";
+// import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Inspect()],
+  plugins: [
+    react(),
+    Inspect(),
+    // visualizer({
+    //   template: "treemap",
+    //   open: true,
+    //   gzipSize: true,
+    //   brotliSize: true,
+    //   filename: "analyze.html",
+    // }),
+  ],
   server: {
     host: true,
   },
