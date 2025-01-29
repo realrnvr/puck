d1ec716a34b44a84bac415e65a635126a1d82752
realrnvr <realrnvr@gmail.com>
2025-01-29 17:49:12 +0530

    useScrollToTopOrSection now conditionally sets scrollBehaviour

--- client/src/hooks/useScrollToTopOrSection.js
+++ client/src/hooks/useScrollToTopOrSection.js
@@ -1,18 +1,24 @@
 import { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 
+const instantScrollPaths = ["/mangas"];
+
 const useScrollToTopOrSection = () => {
   const { pathname, hash } = useLocation();
   const elRef = useRef(null);
 
   useEffect(() => {
+    const scrollBehavior = instantScrollPaths.includes(pathname)
+      ? "instant"
+      : "smooth";
+
     if (hash) {
       elRef.current = document.getElementById(hash.substring(1));
       if (elRef.current) {
-        elRef.current.scrollIntoView({ behavior: "smooth" });
+        elRef.current.scrollIntoView({ behavior: scrollBehavior });
       }
     } else {
-      window.scrollTo({ top: 0, behavior: "smooth" });
+      window.scrollTo({ top: 0, behavior: scrollBehavior });
     }
   }, [pathname, hash]);
 };
