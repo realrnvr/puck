875fbd19634e2204a057ba586d7d3817adf38b7d
realrnvr <realrnvr@gmail.com>
2025-01-30 19:25:53 +0530

    prefetching

--- client/src/pages/read/Read.jsx
+++ client/src/pages/read/Read.jsx
@@ -5,8 +5,10 @@ import { fetchRandomManga } from "../../services/query/query";
 import MangaCard from "../../components/ui/mangaCard/MangaCard";
 import MangaCardError from "../../utils/errors/MangaCardError";
 import MangaCardSkeleton from "../../utils/skeletons/mangaCard/MangaCardSkeleton";
+import { usePrefetchInfiniteManga } from "../../hooks/usePrefetchInfiniteManga";
 
 const LIMIT = 8;
+const MANGAS_LIMIT = Number(import.meta.env.VITE_MANGAS_LIMIT) || 10;
 
 const Read = () => {
   const { data, isPending, isError } = useQuery({
@@ -14,11 +16,17 @@ const Read = () => {
     queryFn: () => fetchRandomManga(LIMIT),
   });
 
+  const { prefetch } = usePrefetchInfiniteManga(MANGAS_LIMIT);
+
   return (
     <div className="read__manga | container">
       <div className="read__text-wrapper">
         <h3 className="read__manga-title">Read manga series!</h3>
-        <Link to="/mangas" className="read__link lg-screen">
+        <Link
+          to="/mangas"
+          className="read__link lg-screen"
+          onMouseEnter={prefetch}
+        >
           See all manga series
         </Link>
       </div>
@@ -37,7 +45,11 @@ const Read = () => {
         {isPending ? <MangaCardSkeleton count={LIMIT} /> : null}
         {isError ? <MangaCardError count={LIMIT} /> : null}
       </div>
-      <Link to="/mangas" className="read__link read__link--mt sm-screen">
+      <Link
+        to="/mangas"
+        className="read__link read__link--mt sm-screen"
+        onMouseEnter={prefetch}
+      >
         See all manga series
       </Link>
     </div>
