1bff63366e10f6962fc8cfd0927f2f7bee4239b0
realrnvr <realrnvr@gmail.com>
2025-01-31 16:37:20 +0530

    test logs removed

--- client/src/pages/auth/redirect/Redirect.jsx
+++ client/src/pages/auth/redirect/Redirect.jsx
@@ -16,15 +16,13 @@ const Redirect = () => {
   const { mutate: googleMutate } = useMutation({
     mutationFn: (code) => axiosInstance.post("/api/v1/auth/google", { code }),
     onSuccess: (data) => {
-      console.log("Response from backend:", data.data);
       auth.setToken(data.data?.accessToken);
-      toast.success("hello :)", { duration: 3000 });
+      toast.success("hello :)");
       navigate("/account");
       isMutating.current = false;
     },
     onError: (error) => {
-      console.error("Error during Google login:", error);
-      toast.error("Something went wrong");
+      toast.error(error?.response?.data?.message || "Something went wrong");
       navigate("/login");
       isMutating.current = false;
     },
@@ -33,13 +31,11 @@ const Redirect = () => {
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const code = params.get("code");
-    console.log("Authorization code:", code);
 
     if (code && !isMutating.current) {
       isMutating.current = true;
       googleMutate(code);
     } else if (!code) {
-      console.error("No authorization code found in the URL.");
       navigate("/login");
     }
   }, [navigate, googleMutate]);
