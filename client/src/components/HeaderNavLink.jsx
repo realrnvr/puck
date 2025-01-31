4206f71bcf9871762846f3d4deaacc5cb3189dfc
realrnvr <realrnvr@gmail.com>
2025-01-31 15:46:49 +0530

    prefetch

--- client/src/components/HeaderNavLink.jsx
+++ client/src/components/HeaderNavLink.jsx
@@ -1,11 +1,18 @@
 import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const HeaderNavLink = ({ value, onClick }) => {
+const HeaderNavLink = ({
+  value,
+  onClick = () => {},
+  onMouseEnter = () => {},
+  onTouchStart = () => {},
+}) => {
   return (
     <li>
       <NavLink
         onClick={onClick}
+        onMouseEnter={onMouseEnter}
+        onTouchStart={onTouchStart}
         style={({ isActive }) =>
           isActive
             ? {
@@ -25,6 +32,8 @@ const HeaderNavLink = ({ value, onClick }) => {
 HeaderNavLink.propTypes = {
   value: PropTypes.string,
   onClick: PropTypes.func,
+  onMouseEnter: PropTypes.func,
+  onTouchStart: PropTypes.func,
 };
 
 export default HeaderNavLink;
