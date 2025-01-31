0bc1a484c5e19b80d4996e8bd590df41b292acf3
realrnvr <realrnvr@gmail.com>
2025-01-31 15:45:34 +0530

    cache added for manga-cover

--- client/src/hooks/usePrefetchFavourite.js
+++ client/src/hooks/usePrefetchFavourite.js
@@ -1,7 +1,8 @@
 import { useQueryClient } from "@tanstack/react-query";
 import { useCallback } from "react";
-import { fetchFavourites } from "../services/query/query";
+import { fetchFavourites, fetchMangaCover } from "../services/query/query";
 import { useAuth } from "./useAuth";
+import toast from "react-hot-toast";
 
 export const usePrefetchFavourite = (LIMIT) => {
   const { token } = useAuth();
@@ -12,13 +13,34 @@ export const usePrefetchFavourite = (LIMIT) => {
       return;
     }
 
-    await queryClient.prefetchInfiniteQuery({
-      queryKey: ["all-favourites", { LIMIT }],
-      queryFn: ({ pageParam = "" }) => fetchFavourites({ LIMIT, pageParam }),
-      initialPageParam: "",
-      getNextPageParam: (lastPage) => lastPage.nextCursor,
-      pages: 1,
-    });
+    try {
+      const response = await queryClient.fetchInfiniteQuery({
+        queryKey: ["all-favourites", { LIMIT }],
+        queryFn: ({ pageParam = "" }) => fetchFavourites({ LIMIT, pageParam }),
+        initialPageParam: "",
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+        pages: 1,
+      });
+
+      response?.pages?.forEach((page) => {
+        page?.client.forEach((val) => {
+          queryClient.prefetchQuery({
+            queryKey: [
+              "manga-cover",
+              { mangaId: val.mangaId, volume: "desc", width: 256 },
+            ],
+            queryFn: () =>
+              fetchMangaCover({
+                mangaId: val.mangaId,
+                volume: "desc",
+                width: 256,
+              }),
+          });
+        });
+      });
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
   }, [queryClient, LIMIT, token]);
 
   return { prefetch };
