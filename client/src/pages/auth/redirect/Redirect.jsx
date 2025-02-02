600dbcbf7cafd784cf3784db970175bfc09fc074
realrnvr <realrnvr@gmail.com>
2025-02-03 00:48:06 +0530

    redirect hook

--- client/src/pages/auth/redirect/Redirect.jsx
+++ client/src/pages/auth/redirect/Redirect.jsx
@@ -1,44 +1,9 @@
 import "./redirect.css";
-import { useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../../hooks/useAuth";
-import { axiosInstance } from "../../../services/api/axios";
-import { useMutation } from "@tanstack/react-query";
-import toast from "react-hot-toast";
+import { useRedirect } from "../../../hooks/useRedirect";
 import Loader from "../../../components/ui/loader/Loader";
 
 const Redirect = () => {
-  const auth = useAuth();
-  const navigate = useNavigate();
-
-  const isMutating = useRef(false);
-
-  const { mutate: googleMutate } = useMutation({
-    mutationFn: (code) => axiosInstance.post("/api/v1/auth/google", { code }),
-    onSuccess: (data) => {
-      auth.setToken(data.data?.accessToken);
-      toast.success("hello :)");
-      navigate("/account");
-      isMutating.current = false;
-    },
-    onError: (error) => {
-      toast.error(error?.response?.data?.message || "Something went wrong");
-      navigate("/login");
-      isMutating.current = false;
-    },
-  });
-
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get("code");
-
-    if (code && !isMutating.current) {
-      isMutating.current = true;
-      googleMutate(code);
-    } else if (!code) {
-      navigate("/login");
-    }
-  }, [navigate, googleMutate]);
+  useRedirect();
 
   return (
     <div className="redirect">
