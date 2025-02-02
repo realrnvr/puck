c83a22ca57011955bb147e13487c6ffa7cc94446
realrnvr <realrnvr@gmail.com>
2025-02-02 12:55:11 +0530

    cookie updates

--- server/helper/setRefreshTokenCookie.js
+++ server/helper/setRefreshTokenCookie.js
@@ -1,8 +1,14 @@
 export const setRefreshTokenCookie = (res, refreshToken) => {
-  res.cookie("refreshToken", refreshToken, {
+  const cookieOptions = {
     httpOnly: true,
     secure: true,
-    SameSite: "Strict",
+    sameSite: "None",
     maxAge: Number(process.env.JWT_REFRESH_TIMESPAN) * 1000,
-  });
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    cookieOptions.domain = process.env.COOKIE_DOMAIN;
+  }
+
+  res.cookie("refreshToken", refreshToken, cookieOptions);
 };
