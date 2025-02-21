1f61451d464fc7b45d0317036ca77f9d8f2fbe08
realrnvr <realrnvr@gmail.com>
2025-02-21 18:41:08 +0530

    redis reconnection config and error handler addition

--- server/config/redisClient.js
+++ server/config/redisClient.js
@@ -6,5 +6,19 @@ export const redisClient = createClient({
   socket: {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
+    reconnectStrategy: function (retries) {
+      if (retries > 20) {
+        console.log(
+          "Too many attempts to reconnect. Redis connection was terminated!"
+        );
+        return new Error("Too many retries.");
+      } else {
+        return retries * 500;
+      }
+    },
   },
+});
+
+redisClient.on("error", (error) => {
+  console.log("Redis Client error:", error);
 });
